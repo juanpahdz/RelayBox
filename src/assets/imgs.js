87312306a1acea04f47a6b3504a4588d1455bfc2
Juan Pablo Hernandez <juanpahdz01@gmail.com>
2021-03-11 21:23:05 -0500

    Add Navbar styles

---
+++ src/assets/imgs.js
@@ -0,0 +1,20 @@
+import Profile from './img/Profile.jpg';
+import all_videos_white from './svg/All_Videos_White.svg';
+import all_videos_active from './svg/All_Videos_Active.svg';
+import get_profile_qr_white from './svg/Get_Profile_QR_White.svg';
+import notifications_white from './svg/Notifications_White.svg';
+import add_new_video_white from './svg/Add_New_Video_White.svg';
+import log_out_white from './svg/Log_Out_White.svg';
+import search_icon from './svg/search_icon.svg'
+
+export default
+    {
+        "profile": Profile,
+        "all_videos_white":all_videos_white,
+        "all_videos_active":all_videos_active,
+        "get_profile_qr_white":get_profile_qr_white,
+        "notifications_white":notifications_white,
+        "add_new_video_white":add_new_video_white,
+        "log_out_white":log_out_white,
+        "search_icon":search_icon,
+    }
--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -1,40 +1,59 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css'
+import imgs from '../../assets/imgs'
+
 
 const Navbar = () => {
     return (
         <header>
             <div className="navbar navbar_container">
+                <div className="close_btn"> </div>
                 <div className="main_title">
-                    BEST VIDEO STORAGE <br /> 
-                    <span>Get in your own a personal video database</span> 
+                    <h4>
+                        BEST VIDEO STORAGE<br/> 
+                        <span>Get in your own a personal video database</span> 
+                    </h4>
                 </div>
 
                 <div className="profile_section">
+
                     <div className="profile_image">
-                        
+                        <div className="img_box">
+                            <img src={imgs.profile} alt=""/>
+                        </div>
                     </div>
+
                     <div className="username_box">
-                        
+                        <div className="profile_name">
+                            <h4>Michael Moore Jong</h4>
+                        </div>
+                        <div className="nickname">
+                            <Link className="nickname_link no_decoration" to="/" >
+                                @michaelmooredit
+                            </Link>
+                        </div>
                     </div>
                     <div className="interactions">
-                        
+                        <button className="btn btn_rounded colorful">
+                            Know about this project
+                        </button>
                     </div>
                 </div>
 
                 <div className="navigation">
                     
                     <nav className="nav_menu">
-                        <Link to="/"> <li className="nav_item active">All Videos</li> </Link>
-                        <Link to="/"> <li className="nav_item">Add a New Video</li> </Link>
-                        <Link to="/"> <li className="nav_item">Notifications</li> </Link>
-                        <Link to="/"> <li className="nav_item">Get Profile QR</li> </Link>
+                        <Link className="link_item no_decoration" to="/"> <li className="nav_item active"> <img className="item_icon" src={imgs.all_videos_active} />All Videos</li> </Link>
+                        <Link className="link_item no_decoration" to="/"> <li className="nav_item"><img className="item_icon" src={imgs.add_new_video_white} /> Add a New Video</li> </Link>
+                        <Link className="link_item no_decoration" to="/"> <li className="nav_item"><img className="item_icon" src={imgs.notifications_white} /> Notifications</li> </Link>
+                        <Link className="link_item no_decoration" to="/"> <li className="nav_item"><img className="item_icon" src={imgs.get_profile_qr_white} /> Get Profile QR</li> </Link>
                     </nav>
                 </div>
 
                 <div className="account_section">
                     <div className="nav_menu">
-                        <Link to="/"> <li className="nav_item active">Log Out <span>Michael</span> </li> </Link>
+                        <Link className="link_item no_decoration" to="/"> <li className="nav_item"> Log Out from <span className="active_user" >Michaelmooredit</span> </li> </Link>
                     </div>
                 </div>
             </div>
